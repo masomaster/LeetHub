4dc35a1f8ec85750faf22a5381fb987d4761ef06
Mason Lancaster <masonlancaster@protonmail.com>
2023-01-26 15:57:17 -0800

    Time: 136 ms (63.62%), Space: 46.6 MB (71.68%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -8,26 +8,9 @@ var romanToInt = function(s) {
     let ints = [1000, 500, 100, 50, 10, 5, 1]
 
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === "C" && s[i+1] === "M") {
-            sum += 900;
-            i++;
-        } else if (s[i] === "C" && s[i+1] === "D") {
-            sum += 400;
-            i++;
-        } else if (s[i] === "X" && s[i+1] === "C") {
-            sum += 90;
-            i++;
-        } else if (s[i] === "X" && s[i+1] === "L") {
-            sum += 40;
-            i++;
-        } else if (s[i] === "I" && s[i+1] === "X") {
-            sum += 9;
-            i++;
-        } else if (s[i] === "I" && s[i+1] === "V") {
-            sum += 4;
-            i++;
-        } else {
-            sum += ints[roman.indexOf(s[i])]
+        sum += ints[roman.indexOf(s[i])]
+        if (roman.indexOf(s[i+1]) !== -1 && roman.indexOf(s[i]) > roman.indexOf(s[i+1])) {
+            sum -= ints[roman.indexOf(s[i])]*2
         }
     }
     return sum;
