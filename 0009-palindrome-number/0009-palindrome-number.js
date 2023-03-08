dcb459f2a708c5944aed12d9c005f43b1c62fc1a
Mason Lancaster <masonlancaster@protonmail.com>
2023-03-07 17:14:59 -0800

    Time: 171 ms (75.63%), Space: 50.8 MB (69.19%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,11 +3,23 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let str = x.toString()
-    let reverse = "";
-    for (let i=str.length-1; i>=0; i--) {
-        reverse = reverse + str[i];
+    
+    // modified after reviewing discussions/solution
+    if (x < 0) return false;
+    let reverse = 0;
+    for (let i = x; i >= 1; i = Math.floor(i/10)) {
+          reverse = reverse*10 + i%10;
     }
-    console.log(reverse, str)
-    return reverse === str;
+    return reverse === x;
 };
+
+// original answer:
+// var isPalindrome = function(x) {
+//     let str = x.toString()
+//     let reverse = "";
+//     for (let i=str.length-1; i>=0; i--) {
+//         reverse = reverse + str[i];
+//     }
+//     console.log(reverse, str)
+//     return reverse === str;
+// };
