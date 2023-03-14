7abb6bfc44d483556385d49ac6a3fecdfcae7173
Mason Lancaster <masonlancaster@protonmail.com>
2023-03-14 11:07:59 -0700

    Time: 370 ms (5.02%), Space: 57.7 MB (5.31%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,16 +2,43 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
+// var isPalindrome = function(x) {
     
-    // modified after reviewing discussions/solution
-    if (x < 0) return false;
-    let reverse = 0;
-    for (let i = x; i >= 1; i = Math.floor(i/10)) {
-          reverse = reverse*10 + i%10;
+//     // modified after reviewing discussions/solution
+//     if (x < 0) return false;
+//     let reverse = 0;
+//     for (let i = x; i >= 1; i = Math.floor(i/10)) {
+//           reverse = reverse*10 + i%10;
+//     }
+//     return reverse === x;
+// };
+
+// recursive solution
+var isPalindrome = function(x) {
+    if (x<0) return false;
+    let numStr = x.toString();
+    if (numStr.length < 2) return true;
+    if (numStr[0] !== numStr[numStr.length-1]) return false;
+    else {
+        numStr = numStr.slice(1, numStr.length-1);
+        console.log(numStr);
+        return isPalindrome(numStr);
     }
-    return reverse === x;
-};
+}
+
+// Or two-pointer version for strings (not integers) which maintains O(n) time complexity but reduces to O(1) space complexity:
+// function isPalindrome(x) {
+//     left = 0;
+//     right = x.length-1;
+//     while (left < right) {
+//         if (x[left] !== x[right]) {
+//             return false;
+//         }
+//         left++;
+//         right--;
+//     }
+//     return true;
+// }
 
 // original answer:
 // var isPalindrome = function(x) {
